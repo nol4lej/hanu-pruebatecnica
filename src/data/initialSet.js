c9509a0e1d659a58d3936b4bbd13d22f102bf71e
nol4lej <nome.ahumada@outlook.com>
2024-05-17 20:33:40 -0400

    Archivo que almacena los set de cartas para jugar.

---
+++ src/data/initialSet.js
@@ -0,0 +1,18 @@
+const initialSet = [
+    { figure: 'spade', value: 'A' },
+    { figure: 'spade', value: '2' },
+    { figure: 'spade', value: '3' },
+    { figure: 'spade', value: '4' },
+
+    { figure: 'diamond', value: 'A' },
+    { figure: 'diamond', value: '2' },
+    { figure: 'diamond', value: '3' },
+    { figure: 'diamond', value: '4' },
+
+    { figure: 'heart', value: 'A' },
+    { figure: 'heart', value: '2' },
+    { figure: 'heart', value: '3' },
+    { figure: 'heart', value: '4' }
+]
+
+export default initialSet
