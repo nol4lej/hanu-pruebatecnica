99173535077e4053a5a3862a47a0064e04a3b488
nol4lej <nome.ahumada@outlook.com>
2024-05-17 20:31:22 -0400

    Componente base Game finalizado: Valor para ordenar agregado, boton para reiniciar, contenedores de Deck y mensaje para jugador ganador. Todo unido junto con el uso del hook useGameLogic

---
+++ src/components/Game/Game.jsx
@@ -0,0 +1,42 @@
+import './Game.css';
+import Deck from '../Deck/Deck';
+import ValueToOrder from '../ValueToOrder/ValueToOrder';
+import RestartButton from '../RestartButton/RestartButton';
+import useGameLogic from '../../hooks/useGameLogic';
+
+const Game = () => {
+
+    const {
+        isWinner,
+        randomValue,
+        leftDeck,
+        centerDeck,
+        rightDeck,
+        handleDeckClick,
+        resetGame,
+    } = useGameLogic();
+
+    return (
+        <div className="game__container">
+            <div className="menu__container">
+                <ValueToOrder value={randomValue} />
+                <RestartButton setters={resetGame} />
+            </div>
+            <div className="decks__container">
+                <div className='decks__content'>
+                    <Deck cards={leftDeck} onClick={handleDeckClick(leftDeck)} />
+                    <Deck cards={centerDeck} onClick={handleDeckClick(centerDeck)} />
+                    <Deck cards={rightDeck} onClick={handleDeckClick(rightDeck)} />
+                </div>
+            </div>
+            {
+                isWinner &&
+                <div className='winner__message__container'>
+                    <p className='winner__message'>EXCELENTE! LOS HAZ LOGRADO!</p>
+                </div>
+            }
+        </div>
+    );
+};
+
+export default Game;
